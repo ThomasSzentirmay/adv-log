070a151f5ed0c394fe5c6869be5fb32853085831
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-03 15:53:13 -0400

    continued file structure

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+
+// Middleware
+
+// Routes
+
+// Start the server
+app.listen(PORT, () => console.log('Server started on port %s', PORT))
